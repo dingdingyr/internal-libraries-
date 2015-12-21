3fd5f3346ccc96bd22bf32d78678c3e564260ab7
dusensong <58247052@qq.com>
2015-12-21 16:51:32 +0800

    remove sth

--- library/src/main/java/cn/wehax/common/framework/model/ResultBean.java
+++
@@ -1,7 +0,0 @@
-package cn.wehax.common.framework.model;
-
-/**
- * Created by mayuhan on 14/12/17.
- */
-public class ResultBean implements IBaseBean {
-}
--- library/src/main/java/cn/wehax/common/framework/view/ISingleView.java
+++
@@ -1,9 +0,0 @@
-package cn.wehax.common.framework.view;
-
-/**
- * Created by Terry on 14/12/19.
- * mail: zhichangterry@gmail.com
- * QQ: 1090035354
- */
-public interface ISingleView extends IContentView {
-}
