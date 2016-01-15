b1ecfdc2090b59eb7114d82efc8a19afa8a460c4
sanchibing <sanchibingxue@sohu.com>
2016-01-15 10:34:39 +0800

    增加工具

---
+++ library/src/main/java/cn/wehax/util/PackageUtils.java
@@ -0,0 +1,98 @@
+package cn.wehax.util;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+
+/**
+ * 提供与Package有关的实用方法
+ */
+public class PackageUtils {
+    public static final String TAG = "PackageUtils";
+
+
+    public static final boolean isInstalled(Activity context, String packageName) {
+        try {
+            context.getPackageManager().getApplicationInfo(packageName, PackageManager.GET_UNINSTALLED_PACKAGES);
+            return true;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    /**
+     * get app version code
+     *
+     * @param context
+     * @return 获取失败返回-select_image_from_native_btn_bg_selector
+     */
+    public static int getAppVersionCode(Context context) {
+        if (context != null) {
+            PackageManager pm = context.getPackageManager();
+            if (pm != null) {
+                PackageInfo pi;
+                try {
+                    pi = pm.getPackageInfo(context.getPackageName(), 0);
+                    if (pi != null) {
+                        return pi.versionCode;
+                    }
+                } catch (NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * get app version name
+     *
+     * @param context
+     * @return 获取失败返回null
+     */
+    public static String getAppVersionName(Context context) {
+        if (context != null) {
+            PackageManager pm = context.getPackageManager();
+            if (pm != null) {
+                PackageInfo pi;
+                try {
+                    pi = pm.getPackageInfo(context.getPackageName(), 0);
+                    if (pi != null) {
+                        return pi.versionName;
+                    }
+                } catch (NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+   /* public static void createShortCut(Activity act, int iconResId, int appnameResId) {
+
+        // com.android.launcher.permission.INSTALL_SHORTCUT
+
+        Intent shortcutintent = new Intent(
+                "com.android.launcher.action.INSTALL_SHORTCUT");
+        // 不允许重复创建
+        shortcutintent.putExtra("duplicate", false);
+        // 需要现实的名称
+        shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_NAME,
+                act.getString(appnameResId));
+        // 快捷图片
+        Parcelable icon = Intent.ShortcutIconResource.fromContext(
+                act.getApplicationContext(), iconResId);
+        shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
+        // 点击快捷图片，运行的程序主入口
+//        shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_INTENT,
+//                new Intent(act.getApplicationContext(), act.getClass()));
+
+        ComponentName comp = new ComponentName(act, SplashActivity.class);
+        shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, new Intent(Intent.ACTION_MAIN).setComponent(comp));
+        // 发送广播
+        act.sendBroadcast(shortcutintent);
+    }*/
+}
